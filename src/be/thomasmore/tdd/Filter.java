71659834aea288b18b29ef91b6613d132ba203ae
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-09-28 21:08:18 +0200

    variabelen gemaakt maar begrijp oefening niet

---
+++ src/be/thomasmore/tdd/Filter.java
@@ -0,0 +1,15 @@
+package be.thomasmore.tdd;
+
+public class Filter {
+    public String size;
+    public int minNrOfPersons;
+    public int maxNrOfPersons;
+
+    public int getMinNrOfPersonsForQuery(){
+        return 404;
+    }
+
+    public int getMaxNrOfPersonsForQuery(){
+        return 404;
+    }
+}
