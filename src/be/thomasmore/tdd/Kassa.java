d0108dc93fac6551565df6be33e3d82e791332d4
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-01 12:04:40 +0200

    oef 5.3 done

--- src/be/thomasmore/tdd/Kassa.java
+++ src/be/thomasmore/tdd/Kassa.java
@@ -1,4 +1,36 @@
 package be.thomasmore.tdd;
 
 public class Kassa {
+    public int getAmountOfCoins(int i) {
+        return i;
+    }
+
+    public int buyCoins(int coinsTable, int amountToAdd) {
+        if (amountToAdd >= 0){
+            return coinsTable + amountToAdd;
+        }else{
+            return coinsTable;
+        }
+
+    }
+
+    public boolean isOrderAllowed(int coinsTable, int amountOrder) {
+        if(coinsTable >= amountOrder){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int order(int coinsTable, int amountOrder) {
+        return coinsTable - amountOrder;
+    }
+
+    public int promotion(int coinsTable) {
+        if(coinsTable >= 10){
+            return coinsTable + 2;
+        }else{
+            return coinsTable;
+        }
+    }
 }
--- src/be/thomasmore/tdd/KassaTest.java
+++ src/be/thomasmore/tdd/KassaTest.java
@@ -7,28 +7,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class KassaTest {
 
     @Test
-    void getAmountOfCoinsForTable(int tableNumber){
-
+    void startingSituation(){
+        Kassa kassa = new Kassa();
+        int coins = kassa.getAmountOfCoins(33);
+        assertEquals(33, coins);
     }
-//
-//    @Test
-//    void buyCoins(int tableNumber, int amountOfCoins){
-//
-//    }
-//
-//    @Test
-//    void isOrderAllowed(int tableNumber, int amountOfCoinsForOrder){
-//
-//    }
-//
-//    @Test
-//    void order(int tableNumber, int amountOfCoinsForOrder){
-//
-//    }
-//
-//    @Test
-//    void promotion(){
-//
-//    }
+
+    @Test
+    void buyCoins(){
+        Kassa kassa = new Kassa();
+        int coins = kassa.getAmountOfCoins(33);
+        coins = kassa.buyCoins(coins, 100);
+        assertEquals(133, coins);
+        coins = kassa.buyCoins(coins, -100);
+        assertEquals(133, coins);
+    }
+
+    @Test
+    void order(){
+        Kassa kassa = new Kassa();
+        int coins = kassa.getAmountOfCoins(33);
+        boolean orderAllowed = kassa.isOrderAllowed(coins, 50);
+        assertFalse(orderAllowed);
+        coins = kassa.buyCoins(coins, 100);
+        assertEquals(133, coins);
+        orderAllowed = kassa.isOrderAllowed(coins, 50);
+        assertTrue(orderAllowed);
+        coins = kassa.order(coins, 50);
+        assertEquals(83, coins);
+    }
+
+
+    @Test
+    void promotion(){
+        Kassa kassa = new Kassa();
+        int coins = kassa.getAmountOfCoins(13);
+        boolean orderAllowed = kassa.isOrderAllowed(coins, 15);
+        assertFalse(orderAllowed);
+        coins = kassa.promotion(coins);
+        assertEquals(15, coins);
+        orderAllowed = kassa.isOrderAllowed(coins, 15);
+        assertTrue(orderAllowed);
+        coins = kassa.order(coins, 15);
+        assertEquals(0, coins);
+        coins = kassa.promotion(coins);
+        assertEquals(0, coins);
+    }
 
 }
