589774eba05de77d5cf4d95f8055a130857d514f
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-01 09:33:11 +0200

    minor changes

--- src/be/thomasmore/tdd/ProductTest.java
+++ src/be/thomasmore/tdd/ProductTest.java
@@ -12,9 +12,9 @@ class ProductTest {
         Product p2 = new Product("product 2", 30, 0);
         Product p3 = new Product("product 3", 0, 6);
         Product p4 = new Product("product 4", 0, 0);
-        assertEquals("30CL 6.2%", p1.productDescription());
-        assertEquals("30CL", p2.productDescription());
-        assertEquals("6%", p3.productDescription());
-        assertEquals("", p4.productDescription());
+        assertEquals("• 30CL 6.2%", p1.productDescription());
+        assertEquals("• 30CL", p2.productDescription());
+        assertEquals("• 6%", p3.productDescription());
+        assertEquals("• ", p4.productDescription());
     }
 }
