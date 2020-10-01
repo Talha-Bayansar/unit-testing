dc1095cd5fe4b860d190116d9d9c774262fc7f72
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-01 10:58:59 +0200

    alle lege functies gemaakt

---
+++ src/be/thomasmore/tdd/Kassa.java
@@ -0,0 +1,4 @@
+package be.thomasmore.tdd;
+
+public class Kassa {
+}
---
+++ src/be/thomasmore/tdd/KassaTest.java
@@ -0,0 +1,34 @@
+package be.thomasmore.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KassaTest {
+
+    @Test
+    void getAmountOfCoinsForTable(int tableNumber){
+
+    }
+//
+//    @Test
+//    void buyCoins(int tableNumber, int amountOfCoins){
+//
+//    }
+//
+//    @Test
+//    void isOrderAllowed(int tableNumber, int amountOfCoinsForOrder){
+//
+//    }
+//
+//    @Test
+//    void order(int tableNumber, int amountOfCoinsForOrder){
+//
+//    }
+//
+//    @Test
+//    void promotion(){
+//
+//    }
+
+}
